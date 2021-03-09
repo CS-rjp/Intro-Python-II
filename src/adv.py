35f5ac8f3a6f49f25dd5155c4809c526d813c67b
Jeannine Proctor <jproctor.m.ed.tn@gmail.com>
2021-03-09 13:10:05 -0600

    add entry point, player and room classes

--- src/adv.py
+++ src/adv.py
@@ -1,19 +1,20 @@
+from player import Player
 from room import Room
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
+    'outside':  Room("Forest Outside the Cave Entrance",
+                     "North of you, the cave mount beckons."),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling.
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west.
 to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
@@ -36,16 +37,23 @@ room['treasure'].s_to = room['narrow']
 #
 # Main
 #
+def adventure_game():
+    print (f'Welcome to the Adventure Game!')
 
-# Make a new player object that is currently in the 'outside' room.
+    # Make a new player object that is currently in the 'outside' room.
+    
 
-# Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+    # Write a loop that:
+    #
+    # * Prints the current room name
+    # * Prints the current description (the textwrap module might be useful here).
+    # * Waits for user input and decides what to do.
+    #
+    # If the user enters a cardinal direction, attempt to move to the room there.
+    # Print an error message if the movement isn't allowed.
+    #
+    # If the user enters "q", quit the game.
+
+
+if __name__ == '__main__':
+  adventure_game()
--- src/room.py
+++ src/room.py
@@ -1,2 +1,10 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, room, descr): # , room, attribute):
+        self.room = room
+        self.descr = descr
+
+    def __str__(self):
+        return (f'You are in the {self.room} room. {self.descr} ')
