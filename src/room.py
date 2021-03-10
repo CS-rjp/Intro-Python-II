67603534d76d6beee29f9d69525891f57b0713cc
Jeannine Proctor <jproctor.m.ed.tn@gmail.com>
2021-03-10 14:16:14 -0600

    add item class, update room class, add psuedo code for player room choice

--- src/room.py
+++ src/room.py
@@ -1,10 +1,25 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
+from item import Items
+
 class Room:
-    def __init__(self, room, descr): # , room, attribute):
-        self.room = room
+    def __init__(self, name, descr): # , room, attribute):
+        """
+        the adventure game has_many rooms
+        """
+        self.name = name
         self.descr = descr
 
     def __str__(self):
-        return (f'You are in the {self.room} room. {self.descr} ')
+        output = f'\t{self.name}'
+
+        return (f'You are in the {self.name} room. {self.descr} ')
+
+        if len(self.items) < 1:
+            return f'There are no items found in {self.name}'
+
+        for item in self.items:
+            output += f'{product}\n'
+
+        return output
